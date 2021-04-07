9d461d4cf0935a460e16e0cc638a9f23715eb757
Gokhan Ozdemir <gokhanozdemir2@outlook.com>
2021-04-08 00:57:01 +0200

    Ajoutez des fichiers projet.

---
+++ MyDictionaryIntro/MyDictionary.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionaryIntro
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+        public void Add(TKey key, TValue value)
+        {
+            bool isExist = ContainsKey(key);
+           
+            if (!isExist)
+            {
+                TKey[] tempKey = keys;
+                TValue[] tempValue = values;
+
+                keys = new TKey[keys.Length + 1];
+                values = new TValue[values.Length + 1];
+
+                for (int i = 0; i < tempKey.Length; i++)
+                {
+                    keys[i] = tempKey[i];
+                    values[i] = tempValue[i];
+                }
+
+                keys[keys.Length - 1] = key;
+                values[values.Length - 1] = value;
+            }
+            else
+            {
+                Console.WriteLine("Ayni" + key + " degerine sahip kayit oldugundan eklenmedi.");
+            }
+           
+        }
+        public bool ContainsValue(TValue value)
+        {
+            bool result=false;
+            foreach (TValue item in values)
+            {
+                result = (item.ToString() == value.ToString()) ? true : false;
+            }
+            return result;
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            bool result = false;
+            foreach (TKey item in keys)
+            {
+                result = (item.ToString() == key.ToString()) ? true : false;
+            }
+            return result;
+        }
+        public void Clear()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+
+        public int Count
+        {
+            get { return keys.Length; }
+        }
+
+        public TKey[] GetKeys
+        {
+            get { return keys; }
+        }
+        public TValue[] GetValues
+        {
+            get { return values; }
+        }
+    }
+}
---
+++ MyDictionaryIntro/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MyDictionaryIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+
+            myDictionary.Add(1, "Gokhan");
+
+            Console.WriteLine(" true or false:"+ myDictionary.ContainsValue("Gokhan"));
+            Console.WriteLine(" true or false:" + myDictionary.ContainsValue("GokhaN"));
+            Console.WriteLine(" true or false:" + myDictionary.ContainsKey(1));
+            Console.WriteLine(" true or false:" + myDictionary.ContainsKey(2));
+
+            myDictionary.Add(1, "Gokhan");
+
+            myDictionary.Clear();
+
+            Console.WriteLine(" true or false:" + myDictionary.ContainsValue("Gokhan"));
+            Console.WriteLine(" true or false:" + myDictionary.ContainsValue("GokhaN"));
+            Console.WriteLine(" true or false:" + myDictionary.ContainsKey(1));
+            Console.WriteLine(" true or false:" + myDictionary.ContainsKey(2));
+
+            myDictionary.Add(1, "Gokhan");
+
+            Console.WriteLine(" true or false:" + myDictionary.ContainsValue("Gokhan"));
+            Console.WriteLine(" true or false:" + myDictionary.ContainsValue("GokhaN"));
+            Console.WriteLine(" true or false:" + myDictionary.ContainsKey(1));
+            Console.WriteLine(" true or false:" + myDictionary.ContainsKey(2));
+
+        }
+    }
+}
